40cee6c895c5ebc23b595445d3de121bcaf96014
acevest <zhaoyanbai@126.com>
2017-04-12 15:49:20 +0800

    quic go client

--- benchmark/client.go
+++ benchmark/client.go
@@ -6,6 +6,7 @@ import (
     "flag"
     "sync"
     "crypto/tls"
+    "math/rand"
     "os"
 
     quic "github.com/lucas-clemente/quic-go"
@@ -20,6 +21,9 @@ type Client struct {
 	cryptoChangedCond sync.Cond
 	session       quic.Session
 	encryptionLevel protocol.EncryptionLevel
+	terminated bool
+    sizeMutex sync.Mutex
+	totalSize int
 }
 
 func (c *Client) Dial() error {
@@ -51,7 +55,7 @@ func (c *Client) connStateCallback(sess quic.Session, state quic.ConnState) {
     }
     switch state {
     case quic.ConnStateVersionNegotiated:
-        utils.Debugf("ConnStateVersionNegotiated")
+        //utils.Debugf("ConnStateVersionNegotiated")
         err := c.versionNegotiateCallback()
         if err != nil {
             c.Close(err)
@@ -69,7 +73,7 @@ func (c *Client) connStateCallback(sess quic.Session, state quic.ConnState) {
 }
 
 func (c *Client) versionNegotiateCallback() error {
-    fmt.Println("versionNegotiateCallback")
+    //fmt.Println("versionNegotiateCallback")
     return nil
 	var err error
 	// once the version has been negotiated, open the header stream
@@ -103,7 +107,7 @@ func (c *Client) handleStream(stream quic.Stream) {
     stream.Close()
 }
 
-func (c *Client) DoRequest() error {
+func (c *Client) DoRequest(PacketSize int) int {
     //fmt.Println(c.encryptionLevel)
     //return nil
     c.mutex.Lock()
@@ -111,35 +115,50 @@ func (c *Client) DoRequest() error {
     for c.encryptionLevel != protocol.EncryptionForwardSecure {
         c.cryptoChangedCond.Wait()
     }
-    fmt.Println("encryptionLevel == protocol.EncryptionForwardSecure")
+    //fmt.Println("encryptionLevel == protocol.EncryptionForwardSecure")
 
     dataStream, err := c.session.OpenStream()
     if err != nil {
         c.Close(err)
         c.mutex.Unlock()
-        return err
+        return 0
     }
     c.mutex.Unlock()
 
     fmt.Println("Open Data StreamId ", dataStream.StreamID())
 
-    buf := make([]byte, 1024)
-    //dataStream.Write([]byte{'x', 'u', 'c'})
-    for i:=0; i<1; i++ {
-        go func() {
-            for {
-                dataStream.Write(buf)
-            }
-        }()
-    }
-    return nil
+	size := 0
+    buf := make([]byte, PacketSize)
+	for i:=0; !c.terminated; i++ {
+		buf[0] = byte(rand.Intn(100))
+		cnt, err := dataStream.Write(buf)
+		if cnt == 0 || err == nil {
+			//size += cnt
+			size = cnt
+			c.sizeMutex.Lock()
+			c.totalSize += size
+			c.sizeMutex.Unlock()
+		} else {
+			fmt.Printf("cnt %v err %v\n", cnt, err)
+			c.terminated = true
+		}
+	}
+
+	fmt.Printf("Stream %d Size %d\n", dataStream.StreamID(), size)
+
+	dataStream.Close()
+
+    return size
 }
 
 func main() {
     fmt.Println("QUIC Client")
     defer fmt.Println("QUIC Client Exit")
 
+	n := flag.Int("n", 1, "stream count")
+	lossRate := flag.Int("r", 0, "loss rate")
 	verbose := flag.Bool("v", false, "verbose")
+	flag.Parse()
 	if *verbose {
 		utils.SetLogLevel(utils.LogLevelDebug)
 	} else {
@@ -172,14 +191,46 @@ func main() {
         TLSConfig: tlsCfg,
     }
     client.cryptoChangedCond = sync.Cond{L: &client.mutex}
-
+	client.terminated = false
     client.Dial()
 
-    go client.DoRequest()
+    var wg sync.WaitGroup
+	const PacketSize = 4096
+	client.totalSize = 0
 
-    for {
-        time.Sleep(1000)
+	bgnTs := time.Now()
+    for i:=0; i<*n; i++ {
+		wg.Add(1)
+        go func(i int) {
+			defer wg.Done()
+			client.DoRequest(PacketSize)
+		}(i)
     }
+
+	go func() {
+		for {
+			deltaTs := float32(time.Since(bgnTs))/float32(time.Second)
+			if deltaTs > 100.0 {
+				client.terminated = true
+				deltaTs := float32(time.Since(bgnTs))/float32(time.Second)
+				fmt.Printf("QUIC Bandwidth %.2f Mbit/s Loss Rate %d%% Total Size %d Delta Time %.2f s\n", float32(client.totalSize)*8/(1024*1024*deltaTs), *lossRate, client.totalSize, deltaTs)
+				client.Close(nil)
+				os.Exit(0)
+				break
+			}
+			time.Sleep(100*time.Millisecond)
+		}
+	}()
+
+
+	//deltaTs := float32(time.Since(bgnTs))/float32(time.Second)
+	//fmt.Printf("QUIC Bandwidth %.2f Mbit/s\n", float32(totalSize)*8/(1024*1024*deltaTs))
+
+	wg.Wait()
+
+	deltaTs := float32(time.Since(bgnTs))/float32(time.Second)
+	fmt.Printf("QUIC Bandwidth %.2f Mbit/s Loss Rate %d%% Total Size %d Delta Time %.2f s\n", float32(client.totalSize)*8/(1024*1024*deltaTs), *lossRate, client.totalSize, deltaTs)
+	client.Close(nil)
 }
 
 func (c *Client) Close(e error) {
